5e1e9e534690aad0dfcf82d4d864a4e827b3a2b4
Md. Iram Faiaz Khan <iram.faiaz99@gmail.com>
2023-05-23 03:34:45 +0600

    first commit

---
+++ src/services/article.js
@@ -0,0 +1,12 @@
+const options = {
+    method: 'GET',
+    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    params: {
+        url: 'https://time.com/6266679/musk-ai-open-letter/',
+        length: '3'
+    },
+    headers: {
+        'X-RapidAPI-Key': 'f77bbcb256msh202b0f03f2b7c6dp1d5051jsnb51574230526',
+        'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+};
